2127afd041a3f682d0c5aeb0fb69382a335a4a78
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2021-05-24 11:55:53 -0600

    Updating files as per assignment

--- src/HelloFromKofi.java
+++ src/HelloFromKofi.java
@@ -1,5 +1,8 @@
 public class HelloFromKofi {
+
     public static void main(String[] args) {
         System.out.println("Hello my name is Kofi.");
+        System.out.println("Hello Kofi, My name is Melissa");
+
     }
 }
